dd2eef28385d88e122872c199993249933e4ac2a
flisowna <ewa.agnieszka.flis@gmail.com>
2022-09-13 15:21:42 +0200

    WIP footer and about us

--- components/sections/Footer.tsx
+++ components/sections/Footer.tsx
@@ -1,9 +1,37 @@
 import Link from "next/link"
+import Facebook from "public/uploads/facebook.svg"
 
 export const Footer = () => {
     return (
-      
-        <div>Footer</div>
+      <section className="bg-navy py-16">
+         <div className="flex flex-col items-center">
+          <img className="h-42" src="/uploads/boxtribute_white.png"/>
+          <div className="flex flex-col">
+          <Link href="/">
+            <a>
+              <img className="h-42" alt="instagram icon" src="/uploads/instagram.svg"/>
+            </a>
+            </Link>
+          <Link href="/">
+            <a>
+              <img className="h-42" alt="facebook icon" src="/uploads/facebook.svg"/>
+            </a>
+            </Link>
+            <Link href="/">
+            <a>
+              <img className="h-42" alt="linkedin icon" src="/uploads/linkedin.svg"/>
+            </a>
+            </Link>
+            <Link href="/">
+            <a>
+              <img className="h-42" alt="github icon" src="/uploads/github.svg"/>
+            </a>
+            </Link>
+          </div>
+        </div>
+<img src='/uploads/facebook.svg'/>
+      </section>
+        
       )
 }
 
--- pages/aboutus.tsx
+++ pages/aboutus.tsx
@@ -1,5 +1,8 @@
 import { getDataBySlug } from "../lib/api";
 import Image from "next/image";
+import 'react-alice-carousel/lib/alice-carousel.css';
+import AliceCarousel from 'react-alice-carousel';
+
 
 type ITextAndImage = {
   image: string;
@@ -7,7 +10,7 @@ type ITextAndImage = {
   description: string;
 };
 
-type IHeroImage = {
+export type IHeroImage = {
   image: string;
   description: string;
 };
@@ -40,47 +43,80 @@ type Props = {
 };
 
 export const AboutUs = ({ aboutUsData }: Props) => {
+
+  const handleDragStart = (e: any) => e.preventDefault();
+
+  const carouselItems = aboutUsData.hero_images_with_description.map(e => 
+    <img src={e.image} alt={e.description} onDragStart={handleDragStart} role="presentation" />,
+    )
+
   return (
     <>
+      {/* hero section */}
       <section>
         <h1 className="text-8xl text-center">
           {aboutUsData.title_of_the_page}
         </h1>
-        <div>
-          {aboutUsData.hero_images_with_description.map((e) => (
-            <Image src={e.image} alt={e.description} width={500} height={500} />
-          ))}
+        <div className="flex justify-center">
+        <AliceCarousel mouseTracking items={carouselItems} />
         </div>
       </section>
+      {/* our story section */}
       <section>
         <div className="mx-32 my-16">
           <h2 className="text-4xl my-8">{aboutUsData.title_section_1}</h2>
-        
-        {aboutUsData.paragraphs_group_intro.map((p) => (
-          <p className="text-2xl my-4">{p.paragraph}</p>
-        ))}
+
+          {aboutUsData.paragraphs_group_intro.map((p, i) => (
+            <p key={i} className="text-2xl my-4">
+              {p.paragraph}
+            </p>
+          ))}
         </div>
-        {aboutUsData.text_with_image_for_the_story.map((e) => (
-          <div className="flex py-16 px-32 justify-between">
-            <div className="flex flex-1 flex-col">
-            {e.paragraph_s_group.map((p) => (
-              <p className="text-2xl my-4">{p.paragraph}</p>
-            ))}
+        {aboutUsData.text_with_image_for_the_story.map((e, i) =>
+          i % 2 == 0 ? (
+            <div key={i} className="flex py-16 px-32 justify-between">
+              <div className="flex flex-1 flex-col">
+                {e.paragraph_s_group.map((p) => (
+                  <p className="text-2xl my-4">{p.paragraph}</p>
+                ))}
+              </div>
+              <div className="flex-2 ml-16">
+                <Image
+                  src={e.image}
+                  alt={e.description}
+                  width={400}
+                  height={400}
+                />
+              </div>
             </div>
-            <div className="flex-2">
-            <Image src={e.image} alt={e.description} width={400} height={400} />
+          ) : (
+            <div key={i} className="flex py-16 px-32 justify-between bg-gray">
+              <div className="flex-2 mr-16">
+                <Image
+                  src={e.image}
+                  alt={e.description}
+                  width={400}
+                  height={400}
+                />
+              </div>
+              <div className="flex flex-1 flex-col ">
+                {e.paragraph_s_group.map((p) => (
+                  <p className="text-2xl my-4">{p.paragraph}</p>
+                ))}
+              </div>
             </div>
-          </div>
-        ))}
+          )
+        )}
       </section>
       <section className="bg-blue py-16 text-2xl">
         <h3 className="text-center mx-48">{aboutUsData.text_legal}</h3>
       </section>
+      {/* team section */}
       <section>
         <h2 className="mx-32 text-4xl p-8">{aboutUsData.title_section_2}</h2>
         <div className="flex flex-wrap justify-center mx-8">
-          {aboutUsData.team_member.map((e) => (
-            <div className="m-4">
+          {aboutUsData.team_member.map((e, i) => (
+            <div key={i} className="m-4">
               <Image
                 src={e.image}
                 alt={e.description}
